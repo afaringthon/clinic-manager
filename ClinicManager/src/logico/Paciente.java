83660937fd165897ea9c066765eb3b8f38ae5d7b
adria <adria@DESKTOP-SVJJ4GE>
2025-11-07 10:13:07 -0600

    setter y getters nombre y apellido

--- ClinicManager/src/logico/Paciente.java
+++ ClinicManager/src/logico/Paciente.java
@@ -1,9 +1,31 @@
 package logico;
 
 public class Paciente {
+	
+	private String nombre; 
+	private String apellido;
+	
+	
+	public Paciente(String nombre, String apellido) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
 
-	public Paciente() {
-		// TODO Auto-generated constructor stub
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 
 }
